ffecfb036cf439b771d7d57691ababed5339dde1
vf27 <victoriafavre7@gmail.com>
2023-09-17 00:38:11 -0300

    agrega archivos login

--- src/app.js
+++ src/app.js
@@ -8,7 +8,9 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
-
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html');
+});
 const port = 3030;
 app.listen(port, () => {
     console.log(`Servidor iniciado en http://localhost:${port}`);
