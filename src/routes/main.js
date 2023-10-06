6c9781c2c2d60749b9bb58c26c7ec3da171d66a3
Triana <trianacamilaperez@gmail.com>
2023-10-05 22:45:40 -0300

    routes

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,7 +1,13 @@
 const express = require('express');
+
 const mainController = require('../controllers/mainController');
+
 const router = express.Router();
 
 router.get('/', mainController.main);
+router.get('/login', mainController.login);
+router.get('/register', mainController.register);
+router.get('/product', mainController.product);
+router.get('/productCart', mainController.productCart);
 
 module.exports = router;
