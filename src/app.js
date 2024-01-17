e1400991bf9beb0a00b77d9869b04a4de327b5c9
leacdc <leacdc@gmail.com>
2024-01-16 23:19:06 -0300

    se agrega express validator

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-
-const app = express();
+const { body, validationResult } = require('express-validator');
 const mainRoutes = require('./routes/main');
 
 app.use(express.static(path.resolve(__dirname, 'public')));
@@ -15,6 +15,13 @@ app.use('/', mainRoutes);
 app.use((req, res, next) => { 
     res.status(404).render('not-found');
 })
+
+app.use(express.json());
+app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
